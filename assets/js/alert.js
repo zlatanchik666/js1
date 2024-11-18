8309c08862b3a97a283ae0cefa4992a0697c9ae7
zlatanchik666 <urazovazlata@gmail.com>
2024-11-18 23:42:14 +0300

    первая версия

---
+++ assets/js/alert.js
@@ -0,0 +1,62 @@
+'use strict'
+
+// |||------1 ПРАКТИКА------|||
+
+
+// ---1 ЗАДАНИЕ---
+
+// alert('Я JavaScript')
+
+
+// ---2 ЗАДАНИЕ---
+
+// let name = prompt('Введите имя', 'Без имени')
+// let surname = prompt('Введите фамилию', 'Без фамилии')
+// let patronymic = prompt('Введите отчество', 'Без отчества')
+// let isTrue = confirm(`Верны ли данные ${name} ${surname} ${patronymic} ?`);
+// alert(isTrue)
+
+
+
+// |||------2 ПРАКТИКА------|||
+
+
+// ---1 ЗАДАНИЕ---
+
+// let name = 'Джон';
+// let admin = 'Джон';
+// alert(admin)
+
+
+// ---2 ЗАДАНИЕ---
+
+// let name = prompt('Введите название города')
+// let agename = prompt('Введите год образования города')
+// let peoplecount = prompt('Введите число населения города')
+// let a = 2024
+// let b = agename
+// let c = a-b
+// let isTrue = confirm(`Городу ${name} исполнилось ${c} лет с момента его образования. Население в размере ${peoplecount} человек`);
+
+
+// ---3 ЗАДАНИЕ---
+
+// let radius = prompt('Введите число радиуса')
+// let r = radius
+// let π = 3.14
+// let s = π*r**2
+// let isTrue = confirm(`Площадь круга с радиусом ${r} равняется ${s} `);
+
+
+// ---4 ЗАДАНИЕ---
+
+// let a = Number(prompt('Введите первое число', 0));
+// let b = +prompt('Введите второе число', 0);
+// alert(a+b);
+// alert(a-b);
+// alert(a/b);
+// alert(a*b);
+
+
+
+
